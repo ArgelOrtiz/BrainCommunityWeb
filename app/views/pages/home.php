5d7bdcf5a6a655e8f209d4e1c0ca1046bfefc31f
ArgelOrtiz <7624000argel>
2019-03-17 11:29:42 -0600

    home banner added

--- app/views/pages/home.php
+++ app/views/pages/home.php
@@ -2,6 +2,8 @@
 
 <div class="mainContainer" >
 
+  <img id="banner" src="<?php echo ROUTE_URL;?>/resource/ejemplo1.jpg" alt="">
+
   <div class="categoryDiv">
     <a href="#" class="active">Categorias</a>
       <?php foreach($data['posts'] as $post): ?>
