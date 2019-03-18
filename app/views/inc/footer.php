b8786f4c2f2b5e9e6ada08d88aacd188e4bc70ed
ArgelOrtiz <7624000argel>
2019-03-17 20:33:52 -0600

    footer created

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,4 +1,11 @@
 
+  <!-- <div class="footer">
+
+    <h6>Brain Community</h6>
+    <h6>Brain Corp.</h6>
+    <h6>braincomunnity@gmail.com</h6>
+
+  </div> -->
 
   <script type="text/javascript" src="<?php echo ROUTE_URL; ?>/js/main.js"></script>
 
