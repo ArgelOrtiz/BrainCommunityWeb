b9a2290c07c04d059205b42414380eb03f6288c5
Aldo Cardona <aldocardonam@hotmail.com>
2019-03-24 11:38:05 -0600

    Post updated

--- app/views/pages/post.php
+++ app/views/pages/post.php
@@ -1,27 +1,61 @@
 <html>
 <head>
-	<title>Item</title>
-
-	<link rel="stylesheet" type="text/css" href="..\Style\General.css"/>
-	<link rel="stylesheet" type="text/css" href="..\Style\Post.css"/>
+	<title>Post</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
-	<div id="Header">
-	</div>
-
-	<div id="left">
-	</div>
-	<div id="right">
-	</div>
-
+	<div id="header"></div>
+	<div id="left"></div>
+	<div id="right"></div>
 	<div id="principal">
-		<div id="question">
-		</div>
-		<div id="answer">
-		</div>
-		<div id="answer2">
-		</div>
+		<div  class="container">
+			<div class="row">
+	            <div align=left class="col-md-12">
+	            	<h4>Categoría</h4>
+	            	<img src="" width=100px height=20px alt="Regresar">
+	            </div>
+	        </div>
+
+	        <div class="container">
+	        	<div style="background:#e6e6e6;" class="row">
+	            	<div style="color:black; font-size: 13px;" class="col-sm">Nombre de usuario</div> 
+	            	<div style="color:black; font-size: 13px;" class="col-sm text-right">Fecha</div>
+	            </div>
+	   			<div style="background:#e6e6e6;" class="row">
+	            	<div style="color:black; font-size: 22px;" class="col-sm ">Pregunta</div>
+	            </div>
+	            <div style="background:#e6e6e6;" class="row">
+	            	<div style="color:black; font-size: 14;" class="col-sm text-justify">Descripción detallada de la pregunta.</div>
+	            </div>
+	            <div style="background:#e6e6e6;" class="row">
+	            	<div style="background:white; color:black; font-size: 12; margin-top:20px; margin-right:20px; margin-bottom:20px;" class="col-sm-2 text-center">Tags</div>
+	            	<div style="background:white; color:black; font-size: 12; margin-top:20px; margin-right:20px; margin-bottom:20px;" class="col-sm-2 text-center">Otro tag</div>
+	            </div>
+	        </div> <!-- Pregunta -->
+
+	        <div style="margin-top:20px; margin-bottom:10px;" class="row">
+	        	<button style="color:black"class="col-sm-4 text-center">Ordenar por</button>
+	        </div>
+	        <!-- for de respuestas -->
+
+	        <div class="container">
+	            <div style="background:#e6e6e6;" class="row">
+	            	<div style="color:black; font-size: 14;" class="col-sm text-justify">Descripción detallada de la pregunta.</div>
+	            </div>
+	            <div style="background:#e6e6e6;" class="row">
+	            	<div style="color:black; font-size: 13px;" class="col-sm">Nombre de usuario</div> 
+	            	<div style="color:black; font-size: 13px;" class="col-sm text-right">Fecha</div>
+	           		
+	           		
+	            </div>
+	            <div style="background:#e6e6e6;" class="row">
+	            	<button style="margin-bottom:20px; margin-left:10px; margin-right:10px;" class="col-sm"> A favor</button>
+	            	<button style="margin-bottom:20px; margin-left:10px; margin-right:10px;" class="col-sm"> En contra</button>
+	            </div>
+	        </div> <!-- Respuesta -->
+
+		</div>  <!-- Container -->
 	</div>
-</body>
+</body> 
 </html>
