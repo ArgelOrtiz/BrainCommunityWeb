132e94c3a659063129fe76d7c00d2dc3c0451c50
ArgelOrtiz <7624000argel>
2019-05-03 11:52:32 -0500

    mounted on the server

--- app/config/config.php
+++ app/config/config.php
@@ -2,15 +2,15 @@
 
   //configtacion de acceso a la base de datos
   define('DB_HOSt','localhost');
-  define('DB_USER','root');
-  define('DB_PASSWORD','');
-  define('DB_NAME','brainommunitytest');
+  define('DB_USER','braincom');
+  define('DB_PASSWORD','H23zscDr0h8WR0f');
+  define('DB_NAME','braincom');
 
   //ruta de la aplicción
   define('ROUTE_APP',dirname(dirname(__file__)));
 
   //ruta url ejemplo: http://localhost/BrainCommunityWeb/
-  define('ROUTE_URL','http://localhost:8080/BrainCommunityWeb');
+  define('ROUTE_URL','http://braincommunity.net/');
 
   //nombre del sitio
   define('NAME_SITE','Brain Community');
--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -10,7 +10,7 @@
   */
 
   class Core{
-    protected $currencyController = 'home';
+    protected $currencyController = 'Home';
     protected $currencyMethod     = 'index';
     protected $parameters         = [];
 
