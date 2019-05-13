ecb97bdd0416835f8ad80795b96377467b938370
ArgelOrtiz <7624000argel>
2019-05-12 23:31:10 -0500

    orejas fixed

--- app/views/pages/newPost.php
+++ app/views/pages/newPost.php
@@ -1,4 +1,17 @@
 ﻿<?php  require ROUTE_APP.'/views/inc/header.php';?>
-<div class="new-posp-container" style="position:absolute; display: flex; top:0px; left:0px; background:red; width:100%; height:calc(100% - 80px); margin:0px; padding:0px; flex-direction:column;">
+<div class="new-post-container" style="position:absolute; display: flex; top:80px; left:0px; background:red; width:100%; height:calc(100% - 80px); margin:0px; padding:0px; flex-direction:column; justify-content: center; align-items: center;">
+
+  <div class="post-container" style="background: green; display:flex; flex-direction:column; height:auto; width:80%;">
+
+    <div class="title-row" style="background: yellow; margin-bottom:15px;">
+      <label>Nueva publicación</label>
+    </div>
+
+    <div class="post-content-row" style="background:blue;">
+      <label>Titulo</label>
+    </div>
+
+
+  </div>
 
 </div>
