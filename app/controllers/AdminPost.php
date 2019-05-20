35b8d2760975a9618b5f54c884133a100d4df570
im batman <34081206+eerv1997@users.noreply.github.com>
2019-05-20 01:22:11 -0500

    Falta redireccion hacia la pagina de creacion de post en admin

--- app/controllers/AdminPost.php
+++ app/controllers/AdminPost.php
@@ -20,6 +20,20 @@ class AdminPost extends Controller
   public function new(){
 
   }
+  public function addpost(){
+    $id = $_POST['post_id'];
+    $post = $this->postModel->adminPosts($user_id, $category_id,$title,$summary,$currentdate);
+    $data = [
+      $titulo = $_POST['title'];
+      $desc = $_POST['summary'];
+      $creacion = $_POST['create_date'];
+      $act = $_POST['last_update'];
+      $coment = $_POST['comments'];
+      $visita = $_POST['visits'];
+      $prior = $_POST['priority'];
+      $estatus = $_POST['status'];
+  ];
+  }
 
 }
 
--- app/views/pages/admin/adminPosts.php
+++ app/views/pages/admin/adminPosts.php
@@ -51,28 +51,28 @@
         <button type="button" class="close" data-dismiss="modal">&times;</button>
         <h4 class="modal-title">Editar</h4>
       </div>
-      <div class="modal-body">
+  <div class="modal-body">
         <!--CONTENIDO-->
-        <label>Nombre</label><br>
+      <label>Nombre</label><br>
       <input style="width:100%" type="text"name="nombreC" id="title"class="form_control" width><br>
       <label>Descripcion</label><br>
-    <input style="width:100%" type="text"name="apellidoM" id="summary"class="form_control"><br>
-    <label>Fecha de Creacion</label><br>
-  <input style="width:100%" type="text"name="apellidoP" id="create_date"class="form_control"><br>
-  <label>Ultima Actualizacion</label><br>
-<input style="width:100%" type="text"name="nac" id="priority"class="form_control"><br>
-<label>Comentarios</label><br>
-<input style="width:100%" type="text"name="nac" id="priority"class="form_control"><br>
-<label>Numero de Visitas</label><br>
-<input style="width:100%" type="text"name="nac" id="priority"class="form_control"><br>
-<label>Prioridad</label><br>
-<input style="width:100%" type="text"name="nac" id="priority"class="form_control"><br>
-<label>Estatus</label><br>
-<input style="width:100%" type="text"name="nac" id="priority"class="form_control"><br>
-<button float:right; type="submit" class="btn btn-info" data-dismiss="modal">Enviar</button>
+      <input style="width:100%" type="text"name="apellidoM" id="summary"class="form_control"><br>
+      <label>Fecha de Creacion</label><br>
+      <input style="width:100%" type="text"name="apellidoP" id="create_date"class="form_control"><br>
+      <label>Ultima Actualizacion</label><br>
+      <input style="width:100%" type="text"name="nac" id="priority"class="form_control"><br>
+      <label>Comentarios</label><br>
+      <input style="width:100%" type="text"name="nac" id="priority"class="form_control"><br>
+      <label>Numero de Visitas</label><br>
+      <input style="width:100%" type="text"name="nac" id="priority"class="form_control"><br>
+      <label>Prioridad</label><br>
+      <input style="width:100%" type="text"name="nac" id="priority"class="form_control"><br>
+      <label>Estatus</label><br>
+      <input style="width:100%" type="text"name="nac" id="priority"class="form_control"><br>
+      <button float:right; type="submit" class="btn btn-info" data-dismiss="modal">Enviar</button>
 
     <!--FIN-->
-      </div>
+  </div>
       <div class="modal-footer">
         <button float:left; type="button" class="btn btn-info" data-dismiss="modal">Cancelar</button>
       </div>
@@ -99,10 +99,10 @@
           <?php foreach($data['categories'] as $category): ?>
             <option value="<?php echo $category->id; ?>"> <?php echo $category->title; ?></option>
           <?php endforeach ?>
-        </select>
+        </select><br><br><br>
   </div>
-  <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal2">Seleccionar
-  </button>
+    <button type="submit" src = "BrainCommunityWeb/app/views/pages/admin/adminNewPost" class="btn btn-info">Seleccionar
+    </button>
     </form>
   </div>
 <!--FIN-->
@@ -113,45 +113,6 @@
 </div>
 <!--FIN MODEL-->
 </div>
-</div>
-<!--MODAL-Contiuacion-->
-  <div id="myModal2" class="modal fade" role="dialog">
-<div class="modal-dialog" >
-<!--MODAL_CONTENT-->
-<div class="modal-content">
-<div class="modal-header">
-  <button type="button" class="close" data-dismiss="modal">&times;</button>
-  <h4 class="modal-title">Agregar Post</h4>
-</div>
-<div class="modal-body">
-  <!--CONTENIDO-->
-  <form class="" action="<?php echo ROUTE_URL.'Post/addpost'?>" method="post">
-  <label>Titulo</label><br>
-<input style="width:100%" type="text"name=title id="title"class="form_control" width><br>
-<label>Descripcion</label><br>
-<input style="width:100%" type="text"name="summary" id="title"class="form_control" width><br>
-<label>Creado</label><br>
-<input style="width:100%" type="text"name="create_date" id="title"class="form_control" width><br>
-<label>Ultima Actualizacion</label><br>
-<input style="width:100%" type="text"name="last_update" id="title"class="form_control" width><br>
-<label>Comentarios</label><br>
-<input style="width:100%" type="text"name="comments" id="title"class="form_control" width><br>
-<label>Visitas</label><br>
-<input style="width:100%" type="text"name="visits" id="title"class="form_control" width><br>
-<label>Prioridad</label><br>
-<input style="width:100%" type="text"name="priority" id="title"class="form_control" width><br>
-<label>Status</label><br>
-<input style="width:100%" type="text"name="status" id="title"class="form_control" width><br><br>
-  <button type="button" class="btn btn-info">Agregar</button>
-  </form>
-</div>
-<!--FIN-->
-</div>
-<div class="modal-footer">
-  <button float:left; type="button" class="btn btn-info" data-dismiss="modal">Cancelar</button>
-</div>
-</div>
-<!--FIN MODEL-->
 </div>
 </div>
 <?php  require ROUTE_APP.'/views/inc/admin/footer.php';?>
