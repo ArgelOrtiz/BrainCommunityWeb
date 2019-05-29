4022e1309d38916809aa01769896f09ad2852c3e
ArgelOrtiz <7624000argel>
2019-05-28 23:04:42 -0500

    view my post updated

--- app/views/pages/myPosts.php
+++ app/views/pages/myPosts.php
@@ -27,58 +27,15 @@
                 <td><?php echo $post->title; ?></td>
                 <td><?php echo $post->create_date; ?></td>
                 <td>
-                  <button class="btn btn-info" type="button" data-toggle="modal" data-target="#post<?php echo $post->id; ?>" name="button">Ver</button>
-                  <button class=" btn btn-danger" type="button"data-toggle="modal"  data-target="#deletePost<?php echo $post->id; ?>">Eliminar</button>
+                  <form class="" action="<?php echo ROUTE_URL.'/Post/viewPost';?>" method="get">
+                    <input type="hidden" name="id" value="<?php echo $post->id; ?>">
+                    <button class="btn btn-info" type="submit" ><li class="glyphicon glyphicon-eye-open"></li></button>
+                    <button class=" btn btn-danger" type="button"data-toggle="modal"  data-target="#deletePost<?php echo $post->id; ?>"><li class="glyphicon glyphicon-trash"></li></button>
+                  </form>
                 </td>
               </tr>
 
-              <!-- view experience Modal -->
-              <div class="modal fade" id="post<?php echo $post->id; ?>" role="dialog">
-                <div class="modal-dialog modal-lg">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h4 class="modal-title">Publicacion</h4>
-                      <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    </div>
-                    <div class="modal-body">
-                      <div class="form-group data-experience-row">
 
-                        <label >Titulo:</label>
-                        <label><?php echo $post->title; ?></label>
-                        <br>
-                        <label>Descripcion:</label>
-                        <label><?php echo $post->summary; ?></label>
-                        <br>
-                        <label>Visitas:</label>
-                        <label><?php echo $post->visits; ?></label>
-                        <br>
-                        <label>Comentarios:</label>
-                        <label><?php echo $post->comments; ?></label>
-                        <br>
-                        <label>Fecha:</label>
-                        <label ><?php echo $post->create_date; ?></label>
-                        <br>
-                        <label>Estatus:</label>
-                        <label>
-                          <?php
-                          if ($post->active == 1) {
-                            // code...
-                            echo "Activo";
-                          }else {
-                            // code...
-                            echo "Oculto";
-                          }
-                          ?>
-                        </label>
-
-                      </div>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-default cancel" data-dismiss="modal">Cancelar</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
 
               <!-- delete post Modal -->
               <div class="modal fade" id="deletePost<?php echo $post->id; ?>" role="dialog">
